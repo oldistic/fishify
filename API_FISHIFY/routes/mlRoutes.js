a35c52ce2fa4bba791d5b3c3022df3fdd2931adc
Michael <michaelariestian@gmail.com>
2023-12-21 09:13:59 +0700

    upadat mlRoutes.js

--- API_FISHIFY/routes/mlRoutes.js
+++ API_FISHIFY/routes/mlRoutes.js
@@ -1,24 +1,20 @@
+const { uploadToCloudStorage } = require('../controllers/mlController');
 const Joi = require('@hapi/joi');
 
 const mlRoutes = [
   {
     method: 'POST',
-    path: '/upload-and-detect-fish',
+    path: '/upload-image',
     options: {
       payload: {
         output: 'stream',
         parse: true,
         allow: 'multipart/form-data',
-        maxBytes: 10 * 1024 * 1024, // Set batas ukuran file (10MB)
+        maxBytes: 10 * 1024 * 1024, // Set file size limit to 10MB
       },
       validate: {
         payload: Joi.object({
-          image: Joi.object({
-            _data: Joi.binary().required(),
-            hapi: Joi.object({
-              filename: Joi.string().regex(/\.(jpg|jpeg|png)$/).required(),
-            }).required(),
-          }).required(),
+          image: Joi.any().required(),
         }),
       },
     },
@@ -28,13 +24,13 @@ const mlRoutes = [
         const fileBuffer = image._data;
         const fileName = image.hapi.filename;
 
-        // Proses gambar dan deteksi ikan
-        const result = await processImageAndDetectFish(fileBuffer, fileName);
+        // Upload the image to Cloud Storage
+        await uploadToCloudStorage(fileBuffer, fileName);
 
-        return h.response(result).code(200);
+        return h.response({ message: 'Image uploaded successfully' }).code(200);
       } catch (error) {
-        console.error('Error processing image:', error);
-        return h.response({ message: 'Terjadi kesalahan server' }).code(500);
+        console.error('Error uploading image:', error);
+        return h.response({ message: 'Server error' }).code(500);
       }
     },
   },
