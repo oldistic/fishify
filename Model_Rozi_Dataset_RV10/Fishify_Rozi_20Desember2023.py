9b4dd05ef862dc203e3b5224469d159e5be4aff3
fachrurozibangkitml28 <143073616+fachrurozibangkitml28@users.noreply.github.com>
2023-12-21 22:11:29 +0700

    Update and rename Fishify_Rozi_14Desember2023.py to Fishify_Rozi_20Desember2023.py
    Add Data_Ikan.csv as additional return value when predict

--- Model_Rozi_Dataset_RV10/Fishify_Rozi_20Desember2023.py
+++ Model_Rozi_Dataset_RV10/Fishify_Rozi_20Desember2023.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.callbacks import EarlyStopping
+
 
 # Define the paths for training and validation data
 train_data_dir = 'train'
@@ -12,12 +15,7 @@ img_height, img_width = 224, 224
 batch_size = 32
 
 # Create data generators with data augmentation for training
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+train_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -35,6 +33,38 @@ valid_generator = valid_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical'
 )
+
+def plot_training_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+
+    # Plot accuracy
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, acc, 'b', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # Plot loss
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 # Load MobileNetV2 pre-trained on ImageNet data
 base_model = MobileNetV2(input_shape=(img_height, img_width, 3),
                         include_top=False,
@@ -56,27 +86,23 @@ model.add(layers.Dense(len(train_generator.class_indices), activation='softmax')
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
-epochs = 50
-target_accuracy = 0.95
-target_val_accuracy = 0.95
+early_stopping = EarlyStopping(
+    monitor='val_accuracy',  # you can also use 'val_loss'
+    patience=4,  # number of epochs with no improvement after which training will be stopped
+    restore_best_weights=True,
+    min_delta=0.03  # minimum change in the monitored metric to qualify as an improvement
+)
 
-for epoch in range(epochs):
-    history = model.fit(
-        train_generator,
-        epochs=1,
-        validation_data=valid_generator
-    )
+# Train the model with early stopping
+history = model.fit(
+    train_generator,
+    epochs=20,
+    validation_data=valid_generator,
+    callbacks=[early_stopping]
+)
 
-    accuracy = history.history['accuracy'][0]
-    val_accuracy = history.history['val_accuracy'][0]
-
-    print(f"Epoch {epoch + 1}/{epochs}")
-
-    # Check if the conditions are met
-    if accuracy >= target_accuracy and val_accuracy >= target_val_accuracy:
-        print(
-            f"Training stopped. Target accuracy ({target_accuracy}) and target validation accuracy ({target_val_accuracy}) reached.")
-        break
+# Plot the training history
+plot_training_history(history)
+  
 # Save the trained model
-model.save('image_classification_model.h5')
+model.save('FishifyModel.h5')
