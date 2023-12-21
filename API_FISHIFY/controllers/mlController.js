1935102bc8ea4ca0c3ecd136ebc56e7480e1b224
Michael <michaelariestian@gmail.com>
2023-12-21 13:30:36 +0700

    upadat mlRoutes.js

--- API_FISHIFY/controllers/mlController.js
+++ API_FISHIFY/controllers/mlController.js
@@ -2,21 +2,21 @@ const { Storage } = require('@google-cloud/storage');
 
 const projectId = 'loyal-world-406507'; 
 const bucketName = 'fishify'; 
-
 const storage = new Storage({ projectId });
 const bucket = storage.bucket(bucketName);
 const cloudStoragePath = 'upload_picture';
 
 const uploadToCloudStorage = async (fileBuffer, fileName) => {
   try {
-    console.log('fileBuffer:', fileBuffer);
+    console.log('Before save - fileBuffer:', fileBuffer);
     if (!fileBuffer) {
-      throw new Error('fileBuffer is undefined or null');
+      throw new Error('Before save - fileBuffer is undefined or null');
     }
 
     const file = bucket.file(`${cloudStoragePath}/${fileName}`);
     await file.save(fileBuffer);
 
+    console.log('After save - fileBuffer:', fileBuffer);
     console.log(`File ${fileName} uploaded to Cloud Storage`);
     return file;
   } catch (error) {
